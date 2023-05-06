f75a0d839296419ce80144bcfed9d76ce77524b0
rainyEra <256truthx@gmail.com>
2023-05-06 06:55:20 +0300

    exp

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { action, atom } from '@reatom/framework'
-import { useAction, useAtom } from '@reatom/npm-react'
-import './App.css'
+import { action, atom } from "@reatom/framework";
+import { useAction, useAtom } from "@reatom/npm-react";
+import "./App.css";
 
-const inputAtom = atom('', 'inputAtom')
+const inputAtom = atom("", "inputAtom");
 const greetingAtom = atom((ctx) => {
-  const input = ctx.spy(inputAtom)
-  return input ? `Hello, ${input}!` : ''
-}, 'greetingAtom')
-const onInput = action((ctx, event: React.ChangeEvent<HTMLInputElement>) =>
-  inputAtom(ctx, event.currentTarget.value),
-  'onInput'
-)
+  const input = ctx.spy(inputAtom);
+  return input ? `Hello, ${input}!` : "";
+}, "greetingAtom");
+const onInput = action(
+  (ctx, event: React.ChangeEvent<HTMLInputElement>) =>
+    inputAtom(ctx, event.currentTarget.value),
+  "onInput"
+);
 
 export default function App() {
-  const [input] = useAtom(inputAtom)
-  const [greeting] = useAtom(greetingAtom)
-  const handleInput = useAction(onInput)
+  const [input] = useAtom(inputAtom);
+  const [greeting] = useAtom(greetingAtom);
+  const handleInput = useAction(onInput);
 
   return (
     <main>
       <p>
-        <input
-          value={input}
-          onChange={handleInput}
-          placeholder="Your name"
-        />
+        <input value={input} onChange={handleInput} placeholder="Your name" />
       </p>
       <p>{greeting}</p>
     </main>
-  )
+  );
 }
